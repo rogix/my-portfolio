91275b5000f635e402ddb3aa91e82457687ce929
Rogerio Pereira <12768403+rogix@users.noreply.github.com>
2026-05-11 17:17:44 -0300

    update date

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -65,7 +65,7 @@ export default function Footer() {
         </li>
       </ul>
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()}
+        © 2024 - {new Date().getFullYear()}
       </p>
     </footer>
   );
